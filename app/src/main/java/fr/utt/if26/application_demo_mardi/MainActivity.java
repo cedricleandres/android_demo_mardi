869fc7abff5c142180c26da45d4a8e64233deb9c
Pierre Groshens <pierre.groshens@gmail.com>
2021-11-30 11:52:07 +0100

    first commit

---
+++ app/src/main/java/fr/utt/if26/application_demo_mardi/MainActivity.java
@@ -0,0 +1,15 @@
+package fr.utt.if26.application_demo_mardi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
